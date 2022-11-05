54f6de2708fd69d1c4e6560a7c3b6e620b86d985
Redstonerayy <antonpaulr@protonmail.com>
2022-11-05 11:17:41 +0100

    added python v2

--- python/README.md
+++ python/README.md
@@ -7,3 +7,25 @@ Selbst dass sortieren von 100 Millionen Integern geht in Python sehr schnell.
 Der großteil der Laufzeit des Programs verfällt sogar auf die Generierung der Zahlen zum sortieren.
 Insgesamt braucht der Code ca 16s, davon 9.4 Generierung und 5.6 Sortierung bei einem Verbraucht von ca. 511MB RAM.
 Wie zu erwarten ist die Implementation der sort() Funktion in Python schon sehr gut. 
+
+# sortv2.x.py
+Eine eigene Implementation von Quicksort in Python schreiben. 
+Erster Versuch, Generation time wie zu erwarten gleich, Sortierzeit
+allerdings 44.6 Sekunden, also deutlich langsamer.
+Nun wird der Code mit numba und numpy verbessert mit numba und numpy.
+Ein numpy aray zu verwenden und numpy.random.randint zur generierung der
+zufälligen Liste verringert die Generierungszeit auf 0.05 Sekunden
+und die Sortiertzeit auf 1.8 Sekunden, somit deutlich schneller als der naive Python
+sort approach ohne die Optimierungen.
+Nun optimiere ich den Code noch weiter.
+Das ändern der Swap funktion und der Pivot element berechnung mach keinen unterschied,
+wahrscheinlich wird das in der Compilierung zu etwas ähnlichem.
+
+python -m compileall <file> sorgt nicht für schnellere laufzeit
+
+Nun teste ich auch nochaml compileall mit der v1. Diese wird daruch auch nicht
+schneller, wahrscheinlich
+
+Im vergleich ist somit diese Version bei der Generierung 5x so langsam wie
+wie die v1 bei cpp und nur halb so langsam beim sortieren.
+
---
+++ python/sortv2.1.py
@@ -0,0 +1,78 @@
+import random
+#random.seed(10) # set seed if desired
+import time
+import numpy as np
+from numba import jit
+import cProfile
+
+# @jit(nopython=True) # makes generation slower but sorting faster?
+def gen(length):
+    return np.random.randint(0, 4294967295, length)
+    # return [random.randint(0, 4294967295) for i in range(length)]
+
+# swap as utility function
+@jit(nopython=True)
+def swap(array_, i1, i2):
+	array_[i1], array_[i2] = array_[i2], array_[i1] 
+	# temp = array_[i1]
+	# array_[i1] = array_[i2]
+	# array_[i2] = temp
+	return array_
+
+# partition
+@jit(nopython=True)
+def hoare_partition(array_, start, end):
+	pivot = array_[(start + end) // 2]
+	# pivot = array_[(start + end) >> 1] # equally fast
+	# pivot = array_[int((start + end) / 2)] # slower
+
+	i = start - 1
+	j = end + 1
+
+	while True:
+		i += 1
+		while array_[i] < pivot:
+			i += 1
+
+		j -= 1
+		while array_[j] > pivot:
+			j -= 1
+
+		if i >= j:
+			return j
+		
+		swap(array_, i, j)
+
+# quicksort
+@jit(nopython=True)
+def quicksort(array_, start, end):
+	if start >= 0 and end >= 0 and start < end:
+		crossing = hoare_partition(array_, start, end)
+
+		quicksort(array_, start, crossing)
+		quicksort(array_, crossing + 1, end)
+
+# length of list to generate and sort
+n = 10 ** 7 # 10 Millions 
+
+# generate and stop time
+genstart = time.time_ns() # get time in nanoseconds to calculate generation time
+liste = gen(n)
+genend = time.time_ns()
+gendiff = genend - genstart
+gendiffseconds = gendiff / (10 ** 9)
+
+# sort and stop time
+sortstart = time.time_ns() # get time in nanoseconds to calculate generation time
+quicksort(liste, 0, len(liste) - 1)
+sortend = time.time_ns()
+sortdiff = sortend - sortstart
+sortdiffseconds = sortdiff / (10 ** 9)
+
+#profile, no useful information over internals of quicksort function
+# with cProfile.Profile(subcalls=True) as pr:
+# 	cProfile.runctx("quicksort(liste, 0, len(liste) - 1)", None, locals())
+
+# output results
+print(f"Generation Time: {gendiff} Nanoseconds or {gendiffseconds} Seconds.")
+print(f"Sorting Time: {sortdiff} Nanoseconds or {sortdiffseconds} Seconds.")
