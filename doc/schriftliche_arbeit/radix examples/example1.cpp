739158e5c928e53efc662e9b84af98eb8f0741ef
Redstonerayy <antonpaulr@protonmail.com>
2023-01-12 09:43:57 +0100

    examples

--- doc/schriftliche_arbeit/radix examples/example1.cpp
+++ doc/schriftliche_arbeit/radix examples/example1.cpp
@@ -1,7 +1,9 @@
-// 
+// Makro, um eines der 4 bytes eines Integers zu extrahieren
+// und als unsigned char zu speichern
 #define RADIXBYTE(num, pass) static_cast<std::uint8_t>((num >> (pass << 3)))
-// 
+// berechnung für jede zu sortierende Zahl
 for(std::uint64_t i = 0; i < vector.size(); ++i){
+	// 
 	counters.at(256 * 0 + RADIXBYTE(vector.at(i), 0))++;
 	counters.at(256 * 1 + RADIXBYTE(vector.at(i), 1))++;
 	counters.at(256 * 2 + RADIXBYTE(vector.at(i), 2))++;
