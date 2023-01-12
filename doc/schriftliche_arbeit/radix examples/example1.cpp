c427d48c6d3d5c7294c372e283900143940ecaa3
Redstonerayy <antonpaulr@protonmail.com>
2023-01-12 14:56:12 +0100

    adding examples

--- doc/schriftliche_arbeit/radix examples/example1.cpp
+++ doc/schriftliche_arbeit/radix examples/example1.cpp
@@ -1,9 +1,9 @@
-// Makro, um eines der 4 bytes eines Integers zu extrahieren
-// und als unsigned char zu speichern
+// C++ Makro, um eines der 4 bytes eines positiven 
+// Integers zu extrahieren und als Byte zu speichern
 #define RADIXBYTE(num, pass) static_cast<std::uint8_t>((num >> (pass << 3)))
-// berechnung für jede zu sortierende Zahl
+// Berechnung für jede zu sortierende Zahl
 for(std::uint64_t i = 0; i < vector.size(); ++i){
-	// 
+	// Zähler des Vorkommens der Bytes erhöhen
 	counters.at(256 * 0 + RADIXBYTE(vector.at(i), 0))++;
 	counters.at(256 * 1 + RADIXBYTE(vector.at(i), 1))++;
 	counters.at(256 * 2 + RADIXBYTE(vector.at(i), 2))++;
--- doc/schriftliche_arbeit/radix examples/example2.cpp
+++ doc/schriftliche_arbeit/radix examples/example2.cpp
@@ -1,10 +1,20 @@
+// Definition der Verschiebungen zur Erhöhung der Zähler
+__m256i_u passes = _mm256_setr_epi16(
+	256 * 3, 256 * 2, 256 * 1, 256 * 0, 
+	256 * 3, 256 * 2, 256 * 1, 256 * 0, 
+	256 * 3, 256 * 2, 256 * 1, 256 * 0, 
+	256 * 3, 256 * 2, 256 * 1, 256 * 0
+);
+// Berechnung für 4 positive Integer auf einmal
 for(std::uint64_t i = 0; i < vectorizedruns; i += 4){
-	
+	// Übertragen der Integer in einen AVX Vector
 	__m256i_u values = _mm256_setr_epi32(
 		0U, vector.at(i), 
 		0U, vector.at(i + 1), 
-		0U, vector.at(i + 2), 0U, 
-		vector.at(i + 3));
+		0U, vector.at(i + 2), 
+		0U, vector.at(i + 3)
+	);
+	// Definition der Verschiebung der Bytes des Vektors "values"
 	__m256i_u shufflevector = _mm256_setr_epi8(
 		0b00000111, 0b10000000, 0b00000110, 0b10000000,
 		0b00000101, 0b10000000, 0b00000100, 0b10000000,
@@ -15,9 +25,11 @@ for(std::uint64_t i = 0; i < vectorizedruns; i += 4){
 		0b00001111, 0b10000000, 0b00001110, 0b10000000,
 		0b00001101, 0b10000000, 0b00001100, 0b10000000
 	);
+	// Verschieben der Bytes im Vector "values"
 	__m256i_u shuffled =  _mm256_shuffle_epi8(values, shufflevector);
+	// Hinzufügen der Verschiebungen durch Addition
 	__m256i_u result = _mm256_adds_epu16(shuffled, passes);
-
+	// Erhöhen der Zähler
 	short *resultpointer = (short*)&result;
 	for(std::uint8_t j = 0; j < 16; j++){
 		counters.at((unsigned short)resultpointer[j])++;
