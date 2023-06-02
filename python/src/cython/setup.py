fedad80b7c19449279d765168d9735f5ce5a8921
Redstonerayy <antonpaulr@protonmail.com>
2023-06-02 15:29:27 +0200

    cython

--- python/src/cython/setup.py
+++ python/src/cython/setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name='Optimized Quicksort',
     ext_modules=cythonize(["quicksort_full_optimization.pyx", "quicksort_with_types.pyx", "quicksort_no_optimization.pyx"]),
-    include_dirs=["./lib/python3.10/site-packages/numpy/core/include"],
+    include_dirs=[numpy.get_include()],
     zip_safe=False,
 )
